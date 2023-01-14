c68409e9eb8e59be9b6114b5184a717c659573ba
Shannon Brown <shannb2012@gmail.com>
2023-01-14 17:25:59 -0500

    Added workaround for whitenoise warning

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,7 @@ if not DEBUG:
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
